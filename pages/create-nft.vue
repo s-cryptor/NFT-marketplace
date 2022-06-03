725c8ac69f447a98d78842a49536d107912e3719
s-cryptor <grisga2001@gmail.com>
2022-06-03 22:22:40 +0300

    add layout for app

---
+++ pages/create-nft.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row justify="center" align="center"> create NFT </v-row>
+</template>
+
+<script>
+export default {
+  name: 'CreateNFTPage',
+}
+</script>
---
+++ pages/my-nfts.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row justify="center" align="center">my NFTs</v-row>
+</template>
+
+<script>
+export default {
+  name: 'MyNFTsPage',
+}
+</script>
